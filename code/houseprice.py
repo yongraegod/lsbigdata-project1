540b5841ea1955d281a29507e1ead92f33706b6c
yongraegod <ohhyunwok@naver.com>
2024-07-29 16:41:51 +0900

    연도별 평균 데이터

--- code/houseprice.py
+++ code/houseprice.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 house_df = pd.read_csv("data/house_price/train.csv")
 house_df.shape
+house_df.info()
 
 price_mean = house_df['SalePrice'].mean()
 
@@ -11,3 +12,33 @@ sub_df
 
 #파일 바꿔치기: csv로 내보내기
 sub_df.to_csv("data/house_price/sample_submission.csv", index = False)
+-----------------------------------------------------------------------
+# train.csv에서 연도별 평균을 구해서, test.csv의 집값을 예측
+
+# 연도 범위, 평균 확인
+house_df['YearBuilt'].describe()
+
+# 년도 별 그룹바이, 가격 평균
+new = house_df.groupby('YearBuilt',as_index = False) \
+              .agg(new_price = ('SalePrice','mean'))
+new
+
+# test.csv 불러오기
+test = pd.read_csv('data/house_price/test.csv')
+
+# test랑 가격 평균을 new2로 합체
+new2 = pd.merge(test, new, how = 'left', on = 'YearBuilt')
+new2.to_csv('data/house_price/new2.csv', index = False)
+new2
+
+# new price 결측치에 전체 평균값넣기
+new2 = new2.fillna(new2['new_price'].mean())
+new2
+
+# 제출용데이터 불러오기
+sub2 = pd.read_csv('data/house_price/sample_submission.csv')
+
+# 제출용 데이터에 년도별 그룹합치기
+sub2['SalePrice'] = new2['new_price']
+sub2.to_csv('data/house_price/sub2.csv', index = False)
+
---
+++ code/houseprice_2.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+house_train = pd.read_csv("data/house_price/train.csv")
+house_train = house_train[["Id","YearBuilt","SalePrice"]]
+house_train.info()
+
+# 연도별 평균
+house_mean = house_train.groupby("YearBuilt", as_index = False) \
+                        .agg(mean_year = ("SalePrice", "mean"))
+house_mean
+
+# test 불러오기
+house_test = pd.read_csv("data/house_price/test.csv")
+house_test = house_test[["Id","YearBuilt"]]
+house_test
+
+# test에 연도별 평균 붙이기
+house_test = pd.merge(house_test, house_mean,
+                      how = "left", on = "YearBuilt")
+house_test = house_test.rename(columns = {"mean_year":"SalePrice"})
+
+# 결측값이 얼마나 있는지 확인
+house_test["SalePrice"].isna().sum()
+# sum(house_test["SalePrice"].isna())
+
+# 비어있는 테스트 세트 집 확인
+house_test.loc[house_test["SalePrice"].isna()]
+
+# 비어있는 값을 평균으로 채우기
+house_mean = house_train["SalePrice"].mean()
+house_test['SalePrice'] = house_test['SalePrice'].fillna(house_mean)
+
+#sub 데이터 불러오기
+sub_df = pd.read_csv("data/house_price/sample_submission.csv")
+
+# SalePrice 바꿔치기
+sub_df['SalePrice'] = house_test['SalePrice']
+sub_df
+
+#파일 바꿔치기: csv로 내보내기
+sub_df.to_csv("data/house_price/sample_submission2.csv", index = False)
